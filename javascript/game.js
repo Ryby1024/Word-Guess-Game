f89fdd999d9bf26fd343b20177d3851242db93b4
Roy Davis <myroy1024@yahoo.com>
2019-10-01 13:17:55 -0400

    getting it working in console log

--- javascript/game.js
+++ javascript/game.js
@@ -1,3 +1,4 @@
+// Getting the text for the html page.
 var directionText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
@@ -5,9 +6,10 @@ var currentWordText = document.getElementById("current-word");
 var guessesLeftText = document.getElementById("guesses-left");
 var lettersUsedText = document.getElementById("letters-used");
 
+
 var wordList = ["sheldon", "leonard", "penny", "raj", "howard", "amy", "bernadette", "bazinga", "stewart", "physics", "astrology", "engineer"];
 
-
+// Assigning variables
 var randomNum = [Math.floor(Math.random() * wordList.length)];
 var word = wordList[randomNum];
 var underScore = [];
@@ -15,15 +17,12 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var wrongLetter = [];
-
+var userChoice;
 var answerArray = [];
-for (var i =0; i < word.length; i++) {
-    underScore.push("_");
-   document.getElementById("current-word").textContent = underScore.join(" ");
-   console.log(word)
-}
+
+// Logging the users key press.
 document.onkeyup = function(e) {
-    userChoice = e.key;
+    userChoice = e.key.toLowerCase();
     if(word.indexOf(userChoice) > -1){
         for(var i = 0; i < word.length; i++){
 
@@ -40,6 +39,18 @@ document.onkeyup = function(e) {
         if(guessesLeft === 0){
             losses++;
         }     
+    for (var i =0; i < word.length; i++) {
+        underScore.push("_");
+       document.getElementById("current-word").textContent = underScore;
+       console.log(word)
     }
     
+    
+    // userChoiceText.textContent = "letters-used: " + userChoice;
+    // winsText.textContent = "Wins: " + wins;
+    // lossesText.textContent = "Losses: " + losses;
+    // guessesLeftText.textContent = "Guesses Left: " - guessesLeftText;
+
+    
     }
+}
