f567461ba6905b8ea6edc5431785181d2e0a827a
Roy Davis <myroy1024@yahoo.com>
2019-10-04 17:57:35 -0400

    got the letters guessed to log

--- javascript/game.js
+++ javascript/game.js
@@ -1,12 +1,3 @@
-// Getting the text for the html page.
-var directionText = document.getElementById("directions-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var currentWordText = document.getElementById("current-word");
-var guessesLeftText = document.getElementById("guesses-left");
-var lettersUsedText = document.getElementById("letters-used");
-
-
 var wordList = ["sheldon", "leonard", "penny", "raj", "howard", "amy", "bernadette", "bazinga", "stewart", "physics", "astrology", "engineer"];
 
 // Assigning variables
@@ -16,47 +7,44 @@ var underScore = [];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var wrongLetter = [];
+var lettersGuessed = [];
 var userChoice;
 var answerArray = [];
 
 // Logging the users key press.
 document.onkeyup = function(e) {
     userChoice = e.key.toLowerCase();
-    if(word.indexOf(userChoice) > -1){
-        for(var i = 0; i < word.length; i++){
+    lettersGuessed.push(userChoice);
+        console.log(lettersGuessed)
+        document.getElementById("letters-used").textContent = "Letters Already Guessed: " + lettersGuessed;
+    console.log(userChoice)
+    for(var i = 0; i < word.length; i++){
+        underScore.push("_");
+           document.getElementById("current-word").textContent = underScore;
+           console.log(word)
+    }
+        if(word.indexOf(userChoice) > -1){
+        
 
-            if (word[i] === userChoice) {
+             if (word[i] === userChoice) {
                 underScore[i] = userChoice;
                 console.log(underScore)
-
-                if(word[i] != userChoice){
-                    guessesLeft--;
-                }
             }
-
-        }
-    }
-    // else {
+                else (word[i] != userChoice) 
+                    underScore[i] = userChoice;
+                    guessesLeft--;
+                 
+            }
+                
+            
+            
+        
+        
+        
+    
         
        
         
-        }     
-    for (var i =0; i < word.length; i++) {
-        underScore.push("_");
-       document.getElementById("current-word").textContent = underScore;
-       console.log(word)
-    }
+            
     
-    
-    // userChoiceText.textContent = "letters-used: " + userChoice;
-    // winsText.textContent = "Wins: " + wins;
-    // lossesText.textContent = "Losses: " + losses;
-    // guessesLeftText.textContent =  10 - guessesLeftText;
-
-    
-    // }
-// }
-
-
-
+}
