2d4e998ceb7be6525b33619586257d0b009e566b
Roy Davis <myroy1024@yahoo.com>
2020-05-28 13:45:14 -0400

    got the calculating a loss working

--- javascript/game.js
+++ javascript/game.js
@@ -1,8 +1,3 @@
-var wordList = ["SHELDON", "LEONARD", "PENNY", "RAJ", "HOWARD", "AMY", "BERNADETTE", "BAZINGA", "STEWART", "PHYSICS", "ASTROLOGY", "ENGINEER"];
-
-// Assigning variables
-var randomNum = [Math.floor(Math.random() * wordList.length)];
-var word = wordList[randomNum];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
@@ -10,33 +5,71 @@ var lettersGuessed = [];
 var userChoice;
 var answerArray = [];
 var underScoreWord = [];
+var positions  =[];
+var word;
+var randomNum;
+var wordList;
 
-
-$(document).ready(function () {
-    console.log(word)
+function letterInWord(letter) {
+    var positions = new Array();
+    for (i = 0 ; i < word.length; i++) {
+        if (word[i] === letter)
+            positions.push(i);
+    }
+    return positions;
+}
+function getWord() {
+    wordList = ["SHELDON", "LEONARD", "PENNY", "RAJ", "HOWARD", "AMY", "BERNADETTE", "BAZINGA", "STEWART", "PHYSICS", "ASTROLOGY", "ENGINEER"];
+    randomNum = [Math.floor(Math.random() * wordList.length)];
+    word = wordList[randomNum];
+    console.log(word);
     for (let i = 0; i < word.length; i++) {
         underScoreWord.push("_");        
     }
     document.getElementById("current-word").innerHTML = underScoreWord.join(" ");
+}
 
-    function letterInWord(letter) {
-        var positions = new Array();
-        for (i = 0 ; i < word.length; i++) {
-            if (word[i] === letter)
-                positions.push(i);
-        }
-        return positions;
+function checkWin() {
+    if(word.length === positions.length) {
+        alert("Congratulations, you got it right!");
+        answerArray = [];
+        guessesLeft = 10;
+        lettersGuessed = [];
+        positions = [];
+        underScoreWord = [];
+        word = "";
+        wins++
+        document.getElementById("wins-text").innerHTML = wins;
+        getWord();
+    }
+}
+function checkGuesses() {
+    if(guessesLeft === 0) {
+        alert("Sorry, you lost! Please try again.")
+        answerArray = [];
+        guessesLeft = 10;
+        lettersGuessed = [];
+        positions = [];
+        underScoreWord = [];
+        word = "";
+        losses++
+        document.getElementById("losses-text").innerHTML = losses;
+        getWord();
     }
 
+}
 
+$(document).ready(function () {
+    getWord();
     // Logging the users key press.
     document.onkeyup = function (e) {
         userChoice = e.key.toUpperCase();
         lettersGuessed.push(userChoice);
-        console.log(lettersGuessed)
+        console.log(lettersGuessed);
         document.getElementById("letters-used").textContent = "Letters Already Guessed: " + lettersGuessed;
-        console.log(userChoice)
-        console.log(word)
+        console.log(userChoice);
+        console.log(word);
+        
 
         var positions = letterInWord(userChoice);
 
@@ -45,14 +78,23 @@ $(document).ready(function () {
 
             for (i = 0 ; i < positions.length; i++) {
                 underScoreWord[positions[i]] = userChoice;
+                answerArray.push(userChoice);
             }
-
             // replace progress Word underscore with letter pressed
             document.getElementById("current-word").innerHTML = underScoreWord.join(" ");
+            guessesLeft--;            
+            document.getElementById("guesses-left").innerHTML = guessesLeft;
+            checkWin();
+            checkGuesses();        
         } else {
-            // alert("WRONG!");
-            document.getElementById("letters-used").innerHTML += userChoice + " ";
-    }
+            guessesLeft--;
+            document.getElementById("guesses-left").innerHTML = guessesLeft;
+            checkGuesses();
+            checkWin();
+        }  
+        
 }
+
+
 
 })
